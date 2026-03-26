92e79bb09d7056c49b367cdba37e5d120dd50123
Ulug'bek Shermetov <u.shermetov11197@gmail.com>
2026-03-26 15:16:25 +0500

    Corrections

--- clinicSADAF/settings.py
+++ clinicSADAF/settings.py
@@ -192,8 +192,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
